ad3293c064a1a4672558a044622cf9ac6f558688
captainlegend007 <emmaekagha6@gmail.com>
2025-06-07 19:23:22 +0100

    "Changes made"

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-// "Changes made""
+import { connectDB } from "./config/db.js";
+import { Person, Testimony } from "./models/Person.js";
+
 const app = express();
 
 const corsOptions = {
@@ -16,47 +18,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 5000;
 
-// await connectDB();
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
 // // Saving Data in MongoDb
-// app.post("/echurch/prayer-request", async (req, res) => {
-//   console.log(req.body);
-//   try {
-//     const { name, number, prayer } = req.body;
-//     const newPerson = new Person({
-//       name,
-//       number,
-//       prayer,
-//     });
-//     await newPerson.save();
-//     console.log(newPerson);
-//     res.send("Person Added");
-//   } catch (error) {
-//     res.send(error.message);
-//   }
-// });
+app.post("/echurch/prayer-request", async (req, res) => {
+  console.log(req.body);
+  try {
+    const { name, number, prayer } = req.body;
+    const newPerson = new Person({
+      name,
+      number,
+      prayer,
+    });
+    await newPerson.save();
+    console.log(newPerson);
+    res.send("Person Added");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-// // Saving Data in MongoDb
-// app.post("/echurch/share-your-testimonies", async (req, res) => {
-//   console.log(req.body);
-//   try {
-//     const { name, number, testimony } = req.body;
-//     const newTestimony = new Testimony({
-//       name,
-//       number,
-//       testimony,
-//     });
-//     await newTestimony.save();
-//     console.log(newTestimony);
-//     res.send("Testimony Added");
-//   } catch (error) {
-//     res.send(error.message);
-//   }
-// });
+// Saving Data in MongoDb
+app.post("/echurch/share-your-testimonies", async (req, res) => {
+  console.log(req.body);
+  try {
+    const { name, number, testimony } = req.body;
+    const newTestimony = new Testimony({
+      name,
+      number,
+      testimony,
+    });
+    await newTestimony.save();
+    console.log(newTestimony);
+    res.send("Testimony Added");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// export const connectDB = async () => {
-//   const MONGODB_URI =
-//     "mongodb+srv://emmaekagha6:Lovejesus1234!@cluster0.9pd6s3v.mongodb.net/ExpressDb";
+export const connectDB = async () => {
+  const MONGODB_URI =
+    "mongodb+srv://emmaekagha6:Lovejesus1234!@cluster0.9pd6s3v.mongodb.net/ExpressDb";
 
-//   await mongoose.connect(MONGODB_URI).then(() => {
-//     console.log("Database Connected");
-//   });
-// };
+  await mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Database Connected");
+  });
+};
