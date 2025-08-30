6ac6a88be4688450ee7627b8937dbe18077ba21e
captainlegend007 <emmaekagha6@gmail.com>
2025-08-30 12:20:31 +0100

    "Changes made"

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.post("/login", async (req, res) => {
 // Handle User Logout
 app.post("/logout", (req, res) => {
   res.clearCookie("token");
-  return res.json({ success: true, status: "success" });
+  return res.json({ success: true, message: "Logged Out" });
 });
 
 app.get("/", (req, res) => {
